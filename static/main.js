fa490cbd947a58dc6d8ace66952faf821aa81574
Dmitry <dmitro02@gmail.com>
2021-12-09 23:29:19 +0200

    manifest

--- static/main.js
+++ static/main.js
@@ -491,7 +491,7 @@ new App().ui.init()
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
       navigator.serviceWorker
-        .register("/serviceWorker.js")
+        .register("./serviceWorker.js")
         .then(res => console.log("service worker registered"))
         .catch(err => console.log("service worker not registered", err))
     })
