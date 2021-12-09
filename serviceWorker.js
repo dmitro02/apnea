731e5d6c2ad58dbcb3c63c81bc31d983fba7e509
Dmitry <dmitro02@gmail.com>
2021-12-09 23:07:26 +0200

    serviceWorker

---
+++ serviceWorker.js
@@ -0,0 +1,31 @@
+const APNEA_CACHE = 'apneaCache'
+
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/static/main.css",
+  "/static/main.js",
+  "/static/icons/android-chrome-512x512.png",
+  "/static/icons/android-chrome-192x192.png",
+  "/static/icons/apple-touch-icon.png",
+  "/static/icons/favicon-16x16.png",
+  "/static/icons/favicon-32x32.png",
+]
+
+self.addEventListener("install", installEvent =>
+  installEvent.waitUntil(
+    caches.open(APNEA_CACHE)
+      .then(cache => cache.addAll(ASSETS))
+  )
+)
+
+self.addEventListener('activate', activateEvent =>
+  console.log("service worker activated", activateEvent)
+)
+
+self.addEventListener("fetch", fetchEvent => 
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request)
+      .then(res => res || fetch(fetchEvent.request))
+  )
+)
