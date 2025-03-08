352ee3b9da64c3bd92ddefca53fb246b5ef4e5be
Dmitro Chebotayov <dmitro02@gmail.com>
2025-03-08 14:53:37 +0200

    update service worker registration to prevent caching during updates

--- static/main.js
+++ static/main.js
@@ -143,7 +143,7 @@ class App {
     registerServiceWorker = () => {
         if ("serviceWorker" in navigator) {
             window.addEventListener("load", () =>
-                navigator.serviceWorker.register("./serviceWorker.js")
+                navigator.serviceWorker.register("./serviceWorker.js", { updateViaCache: 'none' })
             )
         }
     }
